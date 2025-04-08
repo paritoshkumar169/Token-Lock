c17ea9fcc36248cb2606ee3f0e6a104fae30bed8
paritoshkumar169 <paritoshkumar123456@gmail.com>
2025-04-08 17:27:34 +0530

    Updated lib.rs

--- programs/token-lock/src/lib.rs
+++ programs/token-lock/src/lib.rs
@@ -1,62 +1,57 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::{program::invoke, system_instruction};
 
 declare_id!("DNeSEvYcNVwGYruy756J5C2rDMGuTQ2yFhnTkjRHrwYi");
-
-#[account]
-pub struct Vault {
-    pub authority: Pubkey,
-    pub bump: u8,
-}
 
 #[program]
 pub mod token_lock {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        recipient: Pubkey,
+        cancel_permission: u8,
+        change_recipient_permission: u8,
+    ) -> Result<()> {
         let vault = &mut ctx.accounts.vault;
         vault.authority = ctx.accounts.authority.key();
-        vault.bump = ctx.bumps.vault;
+        vault.balance = 0;
+        vault.recipient = recipient;
+        vault.cancel_permission = cancel_permission;
+        vault.change_recipient_permission = change_recipient_permission;
         Ok(())
     }
 
     pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
-        let ix = system_instruction::transfer(
-            &ctx.accounts.user.key(),
-            &ctx.accounts.vault.key(),
+        let vault = &mut ctx.accounts.vault;
+
+        let ix = anchor_lang::solana_program::system_instruction::transfer(
+            &ctx.accounts.user.key,
+            &vault.key(),
             amount,
         );
-
-        invoke(
+        anchor_lang::solana_program::program::invoke(
             &ix,
             &[
                 ctx.accounts.user.to_account_info(),
-                ctx.accounts.vault.to_account_info(),
+                vault.to_account_info(),
             ],
         )?;
 
+        vault.balance += amount;
+
         Ok(())
     }
-    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
-        require_keys_eq!(ctx.accounts.vault.authority, ctx.accounts.authority.key());
-    
-        **ctx.accounts.vault.to_account_info().try_borrow_mut_lamports()? -= amount;
-        **ctx.accounts.recipient.to_account_info().try_borrow_mut_lamports()? += amount;
-    
-        Ok(())
-    }
-    
 }
 
 #[derive(Accounts)]
+#[instruction(recipient: Pubkey, cancel_permission: u8, change_recipient_permission: u8)]
 pub struct Initialize<'info> {
     #[account(
         init,
         seeds = [b"vault", authority.key().as_ref()],
         bump,
         payer = authority,
-        space = 8 + 32 + 1 
-
+        space = 8 + Vault::LEN
     )]
     pub vault: Account<'info, Vault>,
     #[account(mut)]
@@ -66,30 +61,22 @@ pub struct Initialize<'info> {
 
 #[derive(Accounts)]
 pub struct Deposit<'info> {
-    #[account(
-        mut,
-        seeds = [b"vault", user.key().as_ref()],
-        bump = vault.bump
-    )]
+    #[account(mut)]
     pub vault: Account<'info, Vault>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
+#[account]
+pub struct Vault {
+    pub authority: Pubkey,
+    pub balance: u64,
+    pub recipient: Pubkey,
+    pub cancel_permission: u8,
+    pub change_recipient_permission: u8,
+}
 
-#[derive(Accounts)]
-pub struct Withdraw<'info> {
-    #[account(
-        mut,
-        seeds = [b"vault", authority.key().as_ref()],
-        bump = vault.bump
-    )]
-    pub vault: Account<'info, Vault>,
-
-    pub authority: Signer<'info>,
-
-    /// CHECK: This is a regular wallet address. We trust the authority is sending SOL to a valid recipient.
-    #[account(mut)]
-    pub recipient: AccountInfo<'info>,
+impl Vault {
+    pub const LEN: usize = 32 + 8 + 32 + 1 + 1;
 }
